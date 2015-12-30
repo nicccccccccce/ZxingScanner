b610b12495da9c3ed8e581bb2d1bb5e4b65c97d4
nicccccccccce <huxian901001@126.com>
2015-12-30 17:05:41 +0800

    二维码制作工具

---
+++ app/src/main/java/com/ccn/zxing/zcode/ZXingEncodeUtil.java
@@ -0,0 +1,297 @@
+package com.ccn.zxing.zcode;
+
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.PointF;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.util.Hashtable;
+
+/**
+ * Created by eve on 2015/9/10.
+ */
+public class ZXingEncodeUtil {
+
+    //要转换的地址或字符串,可以是中文
+    public static Bitmap createQRImage(String url, int QR_WIDTH, int QR_HEIGHT) {
+        try {
+            //判断URL合法性
+            if (url == null || "".equals(url) || url.length() < 1) {
+                return null;
+            }
+            Hashtable<EncodeHintType, String> hints = new Hashtable<EncodeHintType, String>();
+            hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
+            //图像数据转换，使用了矩阵转换
+            BitMatrix bitMatrix = new QRCodeWriter().encode(url, BarcodeFormat.QR_CODE, QR_WIDTH, QR_HEIGHT, hints);
+            int[] pixels = new int[QR_WIDTH * QR_HEIGHT];
+            //下面这里按照二维码的算法，逐个生成二维码的图片，
+            //两个for循环是图片横列扫描的结果
+            for (int y = 0; y < QR_HEIGHT; y++) {
+                for (int x = 0; x < QR_WIDTH; x++) {
+                    if (bitMatrix.get(x, y)) {
+                        pixels[y * QR_WIDTH + x] = 0xff000000;
+                    } else {
+                        pixels[y * QR_WIDTH + x] = 0xffffffff;
+                    }
+                }
+            }
+            //生成二维码图片的格式，使用ARGB_8888
+            Bitmap bitmap = Bitmap.createBitmap(QR_WIDTH, QR_HEIGHT, Bitmap.Config.ARGB_8888);
+            bitmap.setPixels(pixels, 0, QR_WIDTH, 0, 0, QR_WIDTH, QR_HEIGHT);
+            //显示到一个ImageView上面
+            return bitmap;
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 用于将给定的内容生成成一维码 注：目前生成内容为中文的话将直接报错，要修改底层jar包的内容
+     * @param content 将要生成一维码的内容
+     *
+     * @return 返回生成好的一维码bitmap
+     * @throws WriterException WriterException异常
+     */
+    public static Bitmap CreateOneDCode(String content) throws WriterException {
+        // 生成一维条码,编码时指定大小,不要生成了图片以后再进行缩放,这样会模糊导致识别失败
+        BitMatrix matrix = new MultiFormatWriter().encode(content,
+                BarcodeFormat.CODE_128, 500, 200);
+        int width = matrix.getWidth();
+        int height = matrix.getHeight();
+        int[] pixels = new int[width * height];
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                if (matrix.get(x, y)) {
+                    pixels[y * width + x] = 0xff000000;
+                }
+            }
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(width, height,
+                Bitmap.Config.ARGB_8888);
+        // 通过像素数组生成bitmap,具体参考api
+        bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+        return bitmap;
+    }
+
+    /**
+     * 生成条形码
+     * @param context
+     * @param contents      需要生成的内容
+     * @param desiredWidth  生成条形码的宽带
+     * @param desiredHeight 生成条形码的高度
+     * @param displayCode   是否在条形码下方显示内容
+     *
+     * @return
+     */
+    public static Bitmap creatBarcode(Context context, String contents,
+                                      int desiredWidth, int desiredHeight, boolean displayCode) {
+        Bitmap ruseltBitmap = null;
+        /**
+         * 图片两端所保留的空白的宽度
+         */
+        int marginW = 20;
+        /**
+         * 条形码的编码类型
+         */
+        BarcodeFormat barcodeFormat = BarcodeFormat.CODE_128;
+
+        if (displayCode) {
+            Bitmap barcodeBitmap = encodeAsBitmap(contents, barcodeFormat,
+                    desiredWidth, desiredHeight);
+            Bitmap codeBitmap = creatCodeBitmap(contents, desiredWidth + 2
+                    * marginW, desiredHeight, context);
+            ruseltBitmap = mixtureBitmap(barcodeBitmap, codeBitmap, new PointF(
+                    0, desiredHeight));
+        } else {
+            ruseltBitmap = encodeAsBitmap(contents, barcodeFormat,
+                    desiredWidth, desiredHeight);
+        }
+
+        return ruseltBitmap;
+    }
+
+    /**
+     * 生成条形码的Bitmap
+     * @param contents      需要生成的内容
+     * @param format        编码格式
+     * @param desiredWidth
+     * @param desiredHeight
+     *
+     * @return
+     * @throws WriterException
+     */
+    protected static Bitmap encodeAsBitmap(String contents,
+                                           BarcodeFormat format, int desiredWidth, int desiredHeight) {
+        final int WHITE = 0xFFFFFFFF;
+        final int BLACK = 0xFF000000;
+
+        MultiFormatWriter writer = new MultiFormatWriter();
+        BitMatrix result = null;
+        try {
+            result = writer.encode(contents, format, desiredWidth,
+                    desiredHeight, null);
+        } catch (WriterException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        int width = result.getWidth();
+        int height = result.getHeight();
+        int[] pixels = new int[width * height];
+        // All are 0, or black, by default
+        for (int y = 0; y < height; y++) {
+            int offset = y * width;
+            for (int x = 0; x < width; x++) {
+                pixels[offset + x] = result.get(x, y) ? BLACK : WHITE;
+            }
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(width, height,
+                Bitmap.Config.ARGB_8888);
+        bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+        return bitmap;
+    }
+
+    /**
+     * 生成显示编码的Bitmap
+     * @param contents
+     * @param width
+     * @param height
+     * @param context
+     *
+     * @return
+     */
+    protected static Bitmap creatCodeBitmap(String contents, int width,
+                                            int height, Context context) {
+        TextView tv = new TextView(context);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        tv.setLayoutParams(layoutParams);
+        tv.setText(contents);
+        tv.setHeight(height);
+        tv.setGravity(Gravity.CENTER_HORIZONTAL);
+        tv.setWidth(width);
+        tv.setDrawingCacheEnabled(true);
+        tv.setTextColor(Color.BLACK);
+        tv.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
+                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+        tv.layout(0, 0, tv.getMeasuredWidth(), tv.getMeasuredHeight());
+
+        tv.buildDrawingCache();
+        Bitmap bitmapCode = tv.getDrawingCache();
+        return bitmapCode;
+    }
+
+    /**
+     * 将两个Bitmap合并成一个
+     * @param first
+     * @param second
+     * @param fromPoint 第二个Bitmap开始绘制的起始位置（相对于第一个Bitmap）
+     *
+     * @return
+     */
+    protected static Bitmap mixtureBitmap(Bitmap first, Bitmap second,
+                                          PointF fromPoint) {
+        if (first == null || second == null || fromPoint == null) {
+            return null;
+        }
+        int marginW = 20;
+        Bitmap newBitmap = Bitmap.createBitmap(
+                first.getWidth() + second.getWidth() + marginW,
+                first.getHeight() + second.getHeight(), Bitmap.Config.ARGB_4444);
+        Canvas cv = new Canvas(newBitmap);
+        cv.drawBitmap(first, marginW, 0, null);
+        cv.drawBitmap(second, fromPoint.x, fromPoint.y, null);
+        cv.save(Canvas.ALL_SAVE_FLAG);
+        cv.restore();
+        return newBitmap;
+    }
+
+    //    public  static  Bitmap QRcodeLogo(Bitmap first,Bitmap second,int desWidth,int desHeight){
+//        if (first == null || second == null ) {
+//            return null;
+//        }
+//        int  width=desWidth;
+//        int  height=desHeight;
+//        Bitmap newBitmap =Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_4444);
+//
+//        Canvas cv = new Canvas(newBitmap);
+//
+//        cv.drawBitmap(first,0,0,null);
+//        cv.drawBitmap(first,width*2/5-second.getWidth(),height*2/5-second.getHeight()*2/5,null);
+//        cv.save(Canvas.ALL_SAVE_FLAG);
+//        cv.restore();
+//        return newBitmap;
+//    }
+    // 生成QR图
+    public static Bitmap createQRAndLogo(String text, Bitmap logoBmp, int QR_WIDTH, int QR_HEIGHT) {
+        if (text == null || logoBmp == null || QR_WIDTH == 0 || QR_HEIGHT == 0)
+            return null;
+        try {
+            // 需要引入core包
+            QRCodeWriter writer = new QRCodeWriter();
+            // 把输入的文本转为二维码
+            BitMatrix martix = writer.encode(text, BarcodeFormat.QR_CODE,
+                    QR_WIDTH, QR_HEIGHT);
+
+            //图像数据转换，使用了矩阵转换
+            Hashtable<EncodeHintType, String> hints = new Hashtable<EncodeHintType, String>();
+            hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
+            BitMatrix bitMatrix = new QRCodeWriter().encode(text,
+                    BarcodeFormat.QR_CODE, QR_WIDTH, QR_HEIGHT, hints);
+            int[] pixels = new int[QR_WIDTH * QR_HEIGHT];
+            for (int y = 0; y < QR_HEIGHT; y++) {
+
+//下面这里按照二维码的算法，逐个生成二维码的图片，//两个for循环是图片横列扫描的结果
+                for (int x = 0; x < QR_WIDTH; x++) {
+                    if (bitMatrix.get(x, y)) {
+                        pixels[y * QR_WIDTH + x] = 0xff000000;//黑色
+                    } else {
+                        pixels[y * QR_WIDTH + x] = 0xffffffff;//白色
+                    }
+
+                }
+            }
+            //------------------添加图片部分------------------//
+
+            Bitmap bitmap = Bitmap.createBitmap(QR_WIDTH, QR_HEIGHT,
+                    Bitmap.Config.ARGB_8888);
+
+//设置像素点
+
+            bitmap.setPixels(pixels, 0, QR_WIDTH, 0, 0, QR_WIDTH, QR_HEIGHT);
+
+            Canvas canvas = new Canvas(bitmap);
+            //二维码
+            canvas.drawBitmap(bitmap, 0, 0, null);
+            //图片绘制在二维码中央，合成二维码图片
+            canvas.drawBitmap(logoBmp, bitmap.getWidth() / 2
+                    - logoBmp.getWidth() / 2, bitmap.getHeight()
+                    / 2 - logoBmp.getHeight() / 2, null);
+            //------------------添加logo部分------------------//
+            return bitmap;
+
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+}
